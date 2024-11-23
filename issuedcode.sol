a800cc9c9a1e2e3354e13a0e4986a9e14a51961d
madhu1666827 <122732372+madhu1666827@users.noreply.github.com>
2024-11-23 17:44:04 +0530

    Create issuedcode.sol

---
+++ issuedcode.sol
@@ -0,0 +1,6 @@
+function withdraw() external {
+    uint256 amount = balances[msg.sender];
+    (bool success,) = msg.sender.call{value: balances[msg.sender]}("");
+    require(success);
+    balances[msg.sender] = 0;
+}
